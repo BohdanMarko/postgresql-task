ac42e2fc9bd53b2d237a0c898d1281980f8c7ca5
Bohdan Marko <bohdan.marko.228@gmail.com>
2025-11-27 22:44:27 +0100

    cosmetics

--- create_table_T1.sql
+++ create_table_T1.sql
@@ -1,15 +1,15 @@
-CREATE TABLE [T1] (
-    [Id] SERIAL, -- SERIAL - auto-incrementing integer
-    [Date] DATETIME NOT NULL,
-    [Sum] DECIMAL(10, 2) NOT NULL, -- DECIMAL is an alias for NUMERIC
-    [State] INT NOT NULL, -- mapping for separate enum table
-    [OperationId] UUID NOT NULL,
-    [Message] JSONB NOT NULL DEFAULT '{}'::jsonb, -- for now let's leave empty as default
-    PRIMARY KEY ([Id], [Date])
+CREATE TABLE T1 (
+    Id SERIAL, -- SERIAL - auto-incrementing integer
+    Date DATE NOT NULL,
+    Sum DECIMAL(10, 2) NOT NULL, -- DECIMAL is an alias for NUMERIC
+    State INT NOT NULL, -- mapping for separate enum table
+    OperationId UUID NOT NULL,
+    Message JSONB NOT NULL DEFAULT '{}'::jsonb, -- for now let's leave empty as default
+    PRIMARY KEY (Id, Date)
 ) PARTITION BY RANGE (Date);
 
 CREATE TABLE T1_june2025 PARTITION OF T1
-    FOR VALUES FROM ('2025-06-01') TO ('2025-07-01'); -- [FROM, TO)
+    FOR VALUES FROM ('2025-06-01') TO ('2025-07-01'); -- FROM, TO)
 
 CREATE TABLE T1_july2025 PARTITION OF T1
     FOR VALUES FROM ('2025-07-01') TO ('2025-08-01');
