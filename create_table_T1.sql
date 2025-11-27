b9e5d8136e6aed0d409f375ea4c0053f1e39accf
Bohdan Marko <bohdan.marko.228@gmail.com>
2025-11-27 22:40:12 +0100

    initial commit: create table T1 with partiotions by date

---
+++ create_table_T1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [T1] (
+    [Id] SERIAL, -- SERIAL - auto-incrementing integer
+    [Date] DATETIME NOT NULL,
+    [Sum] DECIMAL(10, 2) NOT NULL, -- DECIMAL is an alias for NUMERIC
+    [State] INT NOT NULL, -- mapping for separate enum table
+    [OperationId] UUID NOT NULL,
+    [Message] JSONB NOT NULL DEFAULT '{}'::jsonb, -- for now let's leave empty as default
+    PRIMARY KEY ([Id], [Date])
+) PARTITION BY RANGE (Date);
+
+CREATE TABLE T1_june2025 PARTITION OF T1
+    FOR VALUES FROM ('2025-06-01') TO ('2025-07-01'); -- [FROM, TO)
+
+CREATE TABLE T1_july2025 PARTITION OF T1
+    FOR VALUES FROM ('2025-07-01') TO ('2025-08-01');
+
+CREATE TABLE T1_august2025 PARTITION OF T1
+    FOR VALUES FROM ('2025-08-01') TO ('2025-09-01');
+
+CREATE TABLE T1_september2025 PARTITION OF T1
+    FOR VALUES FROM ('2025-09-01') TO ('2025-10-01');
